26fced538ce1f38dbbb4bace1eb00e72278af781
Nazmi Altun <nazmialtun@windowslive.com>
2021-04-16 07:49:56 +0800

    First Commit

---
+++ src/TestOkur.Sabit/Startup.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestOkur.Sabit
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
